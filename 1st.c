fd19a680b20501c520a77be4f80ee6a3996c1e23
JKANIMOZHI <kani.rizwan95@gmail.com>
2016-06-06 11:51:47 +0530

    Create 1st.c

---
+++ 1st.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	int num;
+scanf("%d",&num);
+if(num!=0)
+if(num>0)
+printf("Positive");
+else
+printf("Negative");
+else
+printf("Zero");
+	// your code goes here
+	return 0;
+}
