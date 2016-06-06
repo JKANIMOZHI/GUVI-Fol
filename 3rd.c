505a71ef30e98722693fc3b831cf2d2ee9291032
JKANIMOZHI <kani.rizwan95@gmail.com>
2016-06-06 15:07:05 +0530

    Create 3rd.c

---
+++ 3rd.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+char s;
+scanf("%c",&s);
+if(s=='a'|| s=='e' || s=='i' || s=='o' || s=='u')
+printf("vowel");
+else
+printf("consonant");
+	// your code goes here
+	return 0;
+}
