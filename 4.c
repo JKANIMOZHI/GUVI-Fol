518a24382744f38d7fb4554a15724ec4d731ea85
JKANIMOZHI <kani.rizwan95@gmail.com>
2016-06-06 15:21:50 +0530

    Create 4.c

---
+++ 4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+char s;
+scanf("%c",&s);
+if(s>=65&&s<=90 || s>=97&&s<=122)
+printf("alphabet");
+else
+printf("not alphabet");
+	// your code goes here
+	return 0;
+}
