b5f819cd6b43b1c0dc418cd3e9ad6c4c25bd3dbe
JKANIMOZHI <kani.rizwan95@gmail.com>
2016-06-07 14:27:59 +0530

    Create 15.c

---
+++ 15.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	int num1,num2,i;
+	scanf("%d%d",&num1,&num2);
+for(i=num1;i<=num2;i++)
+{
+if(i%2==0)
+printf("%d\n",i);
+}
+	// your code goes here
+	return 0;
+}
