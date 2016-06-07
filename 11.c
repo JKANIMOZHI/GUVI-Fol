9c0dfb8963b876e775d57c1e8a796384fe473802
JKANIMOZHI <kani.rizwan95@gmail.com>
2016-06-07 11:50:55 +0530

    Create 11.c

---
+++ 11.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+	int n,x=0,p=1,power;
+scanf("%d",&n);
+scanf("%d",&power);
+while(power--)
+{
+
+	p=p*n;
+
+}
+
+printf("%d",p);
+	// your code goes here
+	return 0;
+}
+
+   
